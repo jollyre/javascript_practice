216f2703a17e25f38c0e785642cbdceb76bf917c
jollyre <vaityhardik783@gmail.com>
2025-03-27 18:01:44 +0530

    uploaded at 27th March

---
+++ Day1/ex.js
@@ -0,0 +1,18 @@
+
+var a = 10;
+var b =20;
+
+console.log(a+b);
+console.log(b);
+
+function nustarada(){
+    var a = 22;
+    console.log(a);
+}
+
+console.log(nustarada);
+
+for(let i=1; i<10;i++){
+    document.write(i)
+    document.write( )
+}
