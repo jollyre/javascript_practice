82328ddec3af00797f9fce891ab2292b54e8b48e
jollyre <vaityhardik783@gmail.com>
2025-03-27 18:07:09 +0530

    uploaded at 27 march 2025

---
+++ Day2/For Loop/ex1.js
@@ -0,0 +1,20 @@
+let abd = [10, 20,30,40 ,"Hardik"];
+
+console.log(abd);
+
+//for creating object 
+
+let stud ={
+    stud_id : 101,
+    stud_name: "Hardik",
+    addrs: {
+        cityname: "Mumbai",
+        pinCode: 400708,
+        State: "Maharashtra"
+    }
+}
+
+
+console.log(stud);
+console.log(stud.stud_id);
+console.log(stud.addrs.State);
---
+++ Day2/ex1.js
@@ -0,0 +1,32 @@
+
+
+for(i=0;i<=10;i++){
+    console.log("value :: " + i);
+}
+
+let stud = [1,2,3,4,5,];
+
+console.log(stud);
+
+let k = 1;
+let sum =0;
+while (k<= 10){
+    sum+=k;
+    k++;
+}
+
+console.log("sum of the 10 number" + k);
+
+let i = 1;
+
+do {
+  sum += i;
+  i++;
+} while (i <= 10);
+
+console.log("Sum of first 10 natural numbers:", sum);
+
+let weight = 50;
+let age = 15;
+
+    
